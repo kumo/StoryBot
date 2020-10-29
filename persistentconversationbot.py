939d75dce73fbf684ba93c3b2bd24159dc8f4e8d
Rob Clarke <kumo@kumo.it>
2020-10-29 17:00:14 +0100

    Cleaning code to use simple handlers

--- persistentconversationbot.py
+++ persistentconversationbot.py
@@ -32,8 +32,6 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-CHOOSING_STORY, CHOOSING_ACTION = range(2)
-
 stories = {}
 
 def start(update, context):
@@ -44,105 +42,95 @@ def start(update, context):
 
     update.message.reply_text(reply_text, reply_markup=story_markup)
 
-    return CHOOSING_STORY
+    #print(update.effective_chat.id)
 
-
-def choose_story(update, context):
-    text = update.message.text
-    
-    # Keep track of the story
-    context.user_data['story'] = text
-    # And begin the story from the start
-    context.user_data['page'] = 'start'
-    
-    update.message.reply_text("You have chosen {}.".format(text))
-
-    current_page = stories[text]['start']
-
-    # Get the description from the page
-    reply_text = current_page['description']
-
-    # Get the list of options from the page.
-    # We only want to show the text.
-    actions_keyboard = [[action['text']] for action in current_page['options']]
-    markup = ReplyKeyboardMarkup(actions_keyboard, one_time_keyboard=True)
-
-    # Show the description with the list of buttons
-    update.message.reply_text(reply_text, reply_markup=markup, parse_mode='Markdown')
-
-    # Change the state so that we can interact with the story
-    return CHOOSING_ACTION
-
-
-def choose_action(update, context):
-    text = update.message.text
-    
-    # What page did we come from?
-    story = stories[context.user_data.get('story')]
-    previous_page = story[context.user_data.get('page')]
-
-    current_page_name = ""
-    for option in previous_page['options']:
-        if option['text'] == text:
-            current_page_name = option['page']
-            break;
-
-    # Store the current page 
-    context.user_data['page'] = current_page_name
-    
-    # Get the current page from the story
-    current_page = story[current_page_name]
-
-    # Get the description from the page
-    reply_text = current_page['description']
-
-    # Does the page have any options?
-    if 'options' in current_page.keys():
-        # Get the list of options from the page.
-        # We only want to show the text.
-        actions_keyboard = [[action['text']] for action in current_page['options']]
-        markup = ReplyKeyboardMarkup(actions_keyboard, one_time_keyboard=True)
-
-        # Show the description with the list of buttons
-        update.message.reply_text(reply_text, reply_markup=markup, parse_mode='Markdown')
-
-        # Keep the state so that we can interact with the story
-        return CHOOSING_ACTION
-    else:
-        # Create a list of buttons with only a 'Done' button
-        reply_keyboard = [
-            ['Done'],
-        ]
-        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
-
-        # Show the description with the done button
-        update.message.reply_text(reply_text, reply_markup=markup, parse_mode='Markdown')
-
-        if 'ending' in current_page:
-            context.user_data['ending'] = current_page['ending']
-
-        # The player should go back to the start
-        return CHOOSING_STORY
-
-
-def done(update, context):
     if 'page' in context.user_data:
         del context.user_data['page']
     if 'story' in context.user_data:
         del context.user_data['story']
 
-    if 'ending' in context.user_data:
-        if context.user_data['ending'] == 'won':
-            update.message.reply_text("Well done! Until next time!")
 
-        if context.user_data['ending'] == 'death':
-            update.message.reply_text("Better luck next time I hope!")
+def get_page_name_from_choice(page, link):
+    page_name = ""
+    for option in page['options']:
+        if option['text'] == link:
+            page_name = option['page']
+            break;
 
-        del context.user_data['ending']
+    assert(page_name != "")
+    return page_name
+
+
+def end_story(update, context, ending):
+    if 'page' in context.user_data:
+        del context.user_data['page']
+    if 'story' in context.user_data:
+        del context.user_data['story']
+
+    if ending == 'won':
+        ending_message = "Well done! Until next time!"
+
+    if ending == 'death':
+        ending_message = "Better luck next time I hope!"
+
+    # Create a list of buttons with only a 'Done' button
+    reply_keyboard = [
+        ['Done'],
+    ]
+
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+    # Show the description with the done button
+    update.message.reply_text(ending_message, reply_markup=markup, parse_mode='Markdown')
+
+
+def parse_message(update, context):
+    text = update.message.text
+
+    # Is the user currently reading a story?
+    if 'story' in context.user_data:
+        story_name = context.user_data['story']
+        story = stories[story_name]
+
+        # Get the starting page
+        previous_page_name = context.user_data['page']
+        previous_page = story[previous_page_name]
+
+        # Get the next page to read based on the option for the previous page
+        page_name = get_page_name_from_choice(previous_page, text)
     else:
-        update.message.reply_text('See you soon.')
+        # If we don't have a story, the user must have just chosen one.
+        # The user's message contains the name of the story
+        story = stories[text]
+        # Store the name of the story for the user
+        context.user_data['story'] = text
 
-    return ConversationHandler.END
+        # The story begins at the "start" page.
+        page_name = 'start'
+
+    # Get the page and remember it
+    page = story[page_name]
+    context.user_data['page'] = page_name
+
+    # Get the description from the page
+    reply_text = page['description']
+
+    # Is this an ending page?
+    if 'ending' in page:
+        update.message.reply_text(reply_text)
+
+        end_story(update, context, page['ending'])
+        return
+
+    # Does the page have any options?
+    if 'options' in page.keys():
+        # Get the list of options from the page.
+        # We only want to show the text.
+        actions_keyboard = [[action['text']] for action in page['options']]
+        markup = ReplyKeyboardMarkup(actions_keyboard, one_time_keyboard=True)
+
+        # Show the description with the list of buttons
+        update.message.reply_text(reply_text, reply_markup=markup, parse_mode='Markdown')
 
 
 # We need to be able to create folders and list contents of folders
@@ -170,27 +158,14 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # Add conversation handler with the states CHOOSING and TYPING_REPLY
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            CHOOSING_STORY: [
-                MessageHandler(
-                    Filters.text & ~(Filters.command | Filters.regex('^Done$')), choose_story,
-                )
-            ],
-            CHOOSING_ACTION: [
-                MessageHandler(
-                    Filters.text & ~(Filters.command | Filters.regex('^Done$')),
-                    choose_action,
-                )
-            ],
-        },
-        fallbacks=[MessageHandler(Filters.regex('^Done$'), done)],
-        name="my_conversation",
-    )
+    # Add handlers for different commands (for example /start)
+    # the string is the command (without "/") and after there is the function.
+    dp.add_handler(CommandHandler("start", start))
 
-    dp.add_handler(conv_handler)
+    # Add a handler for a normal message (in this case, parse_message)
+    dp.add_handler(MessageHandler(Filters.text & ~(Filters.command | Filters.regex('^Done$')), parse_message))
+
+    dp.add_handler(MessageHandler(Filters.regex('^Done$'), start))
 
     # Load the stories
     load_stories()
